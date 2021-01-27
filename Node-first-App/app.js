b3a411dbb23d03515a45ea479bc37298ddc34d50
tiggy31 <tigist_alemu@fitnyc.edu>
2021-01-27 17:44:46 -0500

    hello

--- Node-first-App/app.js
+++ Node-first-App/app.js
@@ -1,8 +1,12 @@
 
-const http = require(' http')
+const http = require('http')
 
 const server = http.createServer((req,res) => {
-    console.log(req)
+    console.log(req.url, req.method, req.headers)
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<div>')
+    res.end()
 })
 
 server.listen(3000)
