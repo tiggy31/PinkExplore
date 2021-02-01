95b59a73b9d7e8548147230bb2536d7350da97e1
tiggy31 <tigist_alemu@fitnyc.edu>
2021-02-01 18:02:43 -0500

    prior to search com

--- my-git-app/src/App.js
+++ my-git-app/src/App.js
@@ -1,11 +1,24 @@
 import React,{Component} from 'react';
 import NavBar from './Components/layout/Navbar'
 import Users from './Components/users/Users'
+import axios from 'axios'
 import './App.css'
 class App extends Component{
+     state = {
+       users: [],
+       loading: false
+     }
+    async componentDidMount() {
+     this.setState({
+       loading: true
+     })
 
-    componentDidMount() {
-      console.log("hello")
+     const res =  await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+            console.log(res.data)
+            this.setState ({
+              users: res.data,
+              Loading: false
+            })
     }
 
   render(){
@@ -15,7 +28,7 @@ class App extends Component{
          <div>
            <NavBar name="Github Finder"  icon = "fab fa-github"/>
            <div className= "container">
-              <Users />
+              <Users loading={this.state.loading} users={this.state.users}/>
            </div>
          </div>
            
